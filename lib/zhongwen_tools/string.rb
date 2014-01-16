4488e15a98651b769759aad8dfda564f50a51091
Steven Daniels <stevendaniels88@gmail.com>
2014-01-16 12:14:20 +0800

    numbers, tests, string

--- lib/zhongwen_tools/string.rb
+++ lib/zhongwen_tools/string.rb
@@ -5,56 +5,66 @@ require './string/fullwidth'
 module CJKTools
   module String
 
-    def self.split(str)
-      str.scan(/./mu)
+    if RUBY_VERSION < '1.9'
+      def chars(str = nil)
+        (str || self).scan(/./mu)
+      end
+
+      def size(str = nil)
+        chars(str || self).size
+      end
+
+      def reverse(str = nil)
+        chars(str || self).reverse.join
+      end
+
+      def to_utf8(encoding = nil, encodings = nil)
+        #should substitute out known bad actors like space
+        encodings = ['utf-8', 'GB18030', 'BIG5', 'GBK', 'GB2312'] if encodings.nil?
+        encodings = encoding + encodings unless encoding.nil?
+        raise 'Unable to Convert' if encodings.size == 0
+
+        begin
+          text = Iconv.conv('utf-8', encodings[0], self)
+        rescue
+          text = self.zh_to_utf8(nil, encodings[1..-1])
+        end
+        text
+      end
+
+    else 
+      def to_utf8(str = nil)
+        str || self).force_encoding('utf-8')
+        #TODO: better functions available in categorize
+      end
     end
-    def self.length(str)
-      spli(str).size
-    end
-    def self.reverse(str)
-      split(str).reverse.join
-    end
+
     def self.uri_encode
       URI.escape(self, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
     end
-    def to_utf8
-      ic = Iconv.new('UTF-8//IGNORE', 'UTF-8')
-      valid_string = ic.iconv(self + ' ')[0..-2]
-    end
-    # conv = Iconv.new("BIG5//TRANSLIT//IGNORE", "UTF8")
-    # will big5 conversions fail without the options?
-    def zh_to_utf8(encoding = nil, encodings = nil)
-      #should substitute out known bad actors like space
-      encodings = ['utf-8', 'GB18030', 'BIG5', 'GBK', 'GB2312'] if encodings.nil?
-      encodings = encoding + encodings unless encoding.nil?
-      raise 'Unable to Convert' if encodings.size == 0
-
-      begin
-        text = Iconv.conv('utf-8', encodings[0], self)
-      rescue
-        text = self.zh_to_utf8(nil, encodings[1..-1])
-      end
-      text
-    end
 
     def latin?
-      length(str) == str.bytes.count
+      size(str) == str.bytes.count
     end
-    def fullwidth?
-      !self.halfwidth?
-    end
+
     def halfwidth?
       self[/[０-９Ａ-Ｚａ-ｚ％．：＃＄＆＋－／＼＝；＜＞]/].nil?
     end
-    def to_halfwidth(debug = false)
-      matches = self.scan(/([０-９Ａ-Ｚａ-ｚ％．：＃＄＆＋－／＼＝；＜＞])/u).uniq.flatten
-      puts matches.inspect if debug ===true
-      str = self
+
+    def fullwidth?
+      !self.halfwidth?
+    end
+
+    def to_halfwidth(str = nil, debug = false)
+      str = str || self
+      matches = str.scan(/([０-９Ａ-Ｚａ-ｚ％．：＃＄＆＋－／＼＝；＜＞])/u).uniq.flatten
+      puts matches.inspect if debug === true
+
       matches.each do |match|
         replacement = CJKTools::FW_HW[match]
-        puts replacement if debug ===true
-        puts str if debug ===true
-        puts match if debug ===true
+        puts replacement if debug === true
+        puts str if debug === true
+        puts match if debug === true
         str = str.gsub(match, replacement) #unless str.nil?
       end
       str
---
+++ test/test_numbers.rb
@@ -0,0 +1,66 @@
+#encoding: utf-8
+$:.unshift File.join(File.dirname(__FILE__),'..','lib')
+#require 'rubygems'
+  #gem 'guard'
+  #gem 'guard-bundler'
+  #gem 'guard-minitest'
+  #gem 'pry'
+  #gem 'pry-rescue'
+  #gem 'pry-stack_explorer'
+  #gem 'rb-fsevent', :require => false
+
+#require 'bundler'
+#Bundler.require :default, 'test'
+
+require 'test/unit'
+
+require 'zhongwen_tools/numbers.rb'
+
+class TestCJKTools < Test::Unit::TestCase
+  include ZhongwenTools::Numbers
+  def test_convert_to_numbers
+    skip 
+    #your function sucks dick man
+      @numbers.each do |num|
+        number = convert_chinese_numbers_to_numbers num[:zh]
+        assert_equal num[:en], number
+      end
+  end
+
+  def test_convert_to_traditional_number
+    zhs = @numbers[0][:zh]
+    zht = convert_number_to_traditional :zh_s, zhs
+
+    assert_equal '一萬兩千七', zht
+  end
+
+  def test_convert_to_simplified_from_number
+    skip
+   num = @numbers[0][:en]
+    zht = convert_number_to_traditional :num, num
+
+    assert_equal '一萬兩千七', zht
+  end
+
+  def test_convert_number_to_pyn
+    
+    num = '一百三十六'
+    pyn = self.convert_number_to_pyn num
+
+    assert_equal 'yi1-bai2-san1-shi2-liu4', pyn
+  end
+
+  def setup
+    
+    @numbers = [
+      {zh: '一万两千七', en: 12007},
+      {zh: '三千六十三', en: 3063},
+      {zh: '一百五十',en: 150 },
+      {zh: '三千亿', en: 300000000000},
+      {zh: '一九六六', en: 1966},
+      {zh: '二零零八', en: 2008},
+    ]
+  end
+end
+
+
