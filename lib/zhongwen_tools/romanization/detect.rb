cf405d1ae797fe6f591cf243a7b837f7871ef02a
Steven Daniels <stevendaniels88@gmail.com>
2014-05-10 10:42:27 +0800

    Pinyin regex fixes.

--- lib/zhongwen_tools/romanization/detect.rb
+++ lib/zhongwen_tools/romanization/detect.rb
@@ -1,22 +1,21 @@
 # encoding: utf-8
 module ZhongwenTools
   module Romanization
-
+    # https://www.debuggex.com/r/_9kbxA6f00gIGiVo
     pyn_regexes = {
       :bpm_regex => /([mM]iu|[pmPM]ou|[bpmBPM](o|e(i|ng?)?|a(ng?|i|o)?|i(e|ng?|a[no])?|u))/,
       :f_regex => /([fF](ou?|[ae](ng?|i)?|u))/,
       :dt_regex => /([dD](e(i|ng?)|i(a[on]?|u))|[dtDT](a(i|ng?|o)?|e(i|ng)?|i(a[on]?|e|ng|u)?|o(ng?|u)|u(o|i|an?|n)?))/,
       :nl_regex => /([nN]eng?|[lnLN](a(i|ng?|o)?|e(i|ng)?|i(ang|a[on]?|e|ng?|u)?|o(ng?|u)|u(o|i|an?|n)?|ve?))/,
       :gkh_regex => /([ghkGHK](a(i|ng?|o)?|e(i|ng?)?|o(u|ng)|u(a(i|ng?)?|i|n|o)?))/,
-      :zczhch_regex => /([zZ][h]?ei|[czCZ]hua(i|ng?)?|[czCZ][h]?(a(i|ng?|o)?|en?g?|o(u|ng)?|u(a?n|o|i)?))/,
-      :ssh_regex => /([sS]ong|[sS]hua(i|ng?)?|[sS]hei|[sS][h]?(a(i|ng?|o)?|en?g?|ou|u(a?n|o|i)?))/,
+      :zczhch_regex => /([zZ]h?ei|[czCZ]h?(e(ng?)?|o(ng?|u)?|ao|u?a(i|ng?)?|u?(o|i|n)?))/,
+      :ssh_regex => /([sS]ong|[sS]hua(i|ng?)?|[sS]hei|[sS][h]?(a(i|ng?|o)?|en?g?|ou|u(a?n|o|i)?|i))/,
       :r_regex => /([rR]([ae]ng?|i|e|ao|ou|ong|u[oin]|ua?n?))/,
       :jqx_regex => /([jqxJQX](i(a(o|ng?)?|[eu]|ong|ng?)?|u(e|a?n)?))/,
-      :aeo_regex => /(([aA](i|o|ng?)?|[oO]u?|[eE](i|ng?)?))/,
+      :aeo_regex => /(([aA](i|o|ng?)?|[oO]u?|[eE](i|ng?|r)?))/,
       :w_regex => /([wW](a(i|ng?)?|o|e(i|ng?)?|u))/,
       :y_regex => /[yY](a(o|ng?)?|e|in?g?|o(u|ng)?|u(e|a?n)?)/
     }
-
 
     if RUBY_VERSION < '1.9'
       py_tones = {
