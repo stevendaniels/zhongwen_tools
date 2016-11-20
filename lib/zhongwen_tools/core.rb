45fca9fda353c5a93a96f1ba5e32eecc6673cdb1
Steven Daniels <stevendaniels88@gmail.com>
2016-11-20 08:02:15 -0500

    Added respond_to? method for refinements
    According to Ruby Docs
    > When using indirect method access such as Kernel#send, Kernel#method, or Kernel#respond_to?, refinements are not honored for the caller context during method lookup. This behavior may be changed in the future.

--- lib/zhongwen_tools/core.rb
+++ lib/zhongwen_tools/core.rb
@@ -1,20 +1,24 @@
 # encoding: utf-8
-require 'zhongwen_tools/ruby_19' if RUBY_VERSION < '2.0.0'
-require 'zhongwen_tools/regex'
-require 'zhongwen_tools/fullwidth'
-require 'zhongwen_tools/unicode'
-require 'zhongwen_tools/uri'
-require 'zhongwen_tools/zhongwen'
-require 'zhongwen_tools/number'
-require 'zhongwen_tools/romanization'
-require 'zhongwen_tools/string_extension'
-require 'zhongwen_tools/version'
+require "zhongwen_tools/ruby_19" if RUBY_VERSION < "2.0.0"
+require "zhongwen_tools/regex"
+require "zhongwen_tools/fullwidth"
+require "zhongwen_tools/unicode"
+require "zhongwen_tools/uri"
+require "zhongwen_tools/zhongwen"
+require "zhongwen_tools/number"
+require "zhongwen_tools/romanization"
+require "zhongwen_tools/string_extension"
+require "zhongwen_tools/version"
 
 module ZhongwenTools
   # Allow for the use Refinements.
-  if RUBY_VERSION >= '2.0.0'
+  if RUBY_VERSION >= "2.0.0"
     refine String do
       include ::ZhongwenTools::StringExtension
+
+      def respond_to?(method, include_private = false)
+        super || ::ZhongwenTools::StringExtension.instance_methods.include?(method)
+      end
     end
   end
 end
