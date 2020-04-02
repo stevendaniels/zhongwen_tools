c6a511d5f9ca8b787d6d43e2a92e3f749ef1759c
Steven Daniels <stevendaniels88@gmail.com>
2020-04-01 22:47:57 -0400

    Fix flakey tests
    Test were failing because of the order of the core test. It removes a module from the object, which broke things. Reloading the module after the test runs should fix things.

--- test/test_core.rb
+++ test/test_core.rb
@@ -4,10 +4,13 @@ $:.unshift File.join(File.dirname(__FILE__),'..','lib')
 require './test/test_helper'
 
 class TestCore < Minitest::Test
+
   def test_core_ext_does_not_have_script_functions
     load 'test_helpers/unload_zhongwen_tools_script.rb'
     require 'zhongwen_tools/core'
     require 'zhongwen_tools/core_ext/string'
     assert_raises(NoMethodError){ '你们'.to_zht }
+    # NOTE: restoring ZhongwenTools::Script
+    load 'zhongwen_tools/script.rb'
   end
 end
